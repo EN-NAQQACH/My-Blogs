c405c555d5aa9ad3590aacace90e9e34c8388549
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-07-12 23:50:12 +0100

    auth Providers

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -1,6 +1,10 @@
 import Google from "next-auth/providers/google"
 import Facebook from 'next-auth/providers/facebook'
 import connectDB from "../../../server/libs/Mongodb"
+import { User } from '../../../server/models/users'
+import Credentials from "next-auth/providers/credentials"
+import bcrypt from 'bcrypt'
+
 
 export const authOptions = {
   providers: [
@@ -12,15 +16,69 @@ export const authOptions = {
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
     }),
-  ], 
+
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' }
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        await connectDB();
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error('No user found with the email');
+        }
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+          throw new Error('Invalid password');
+        }
+        return { id: user._id, name: user.name, email: user.email };
+      }
+    })
+  ],
 
   callbacks: {
-     async signIn(user, account, profile) {
-      console.log(user, account, profile)
-        // do just connecting to mongodb
-        await connectDB()
-        return true
+    async signIn({ user, account, profile }) {
+      //console.log(user, account, profile)
+
+      if (account.provider === 'google') {
+        try {
+          await connectDB()
+          const user = await User.findOne({ email: profile.email })
+          if (!user) {
+            const newUser = new User({
+              email: profile.email,
+              name: profile.name,
+              image: profile.image,
+            })
+            await newUser.save()
+          }
+        } catch (err) {
+          console.log(err)
+          return false
+        }
+      }
+      if (account.provider === 'facebook') {
+        try {
+          await connectDB()
+          const user2 = await User.findOne({ email: profile.email })
+          if (!user2) {
+            const newUser = new User({
+              email: user.email,
+              name: user.name,
+              image: user.image,
+            })
+            await newUser.save()
+          }
+        } catch (err) {
+          console.log(err)
+          return false
+        }
+      }
+      return true
     },
-    }
-  
+  }
+
 }
