76b207ba5f60d1e156cba01ef6b5d58a316bbbbc
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-07-15 21:59:54 +0100

    login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,7 +5,11 @@ import Link from 'next/link';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { paraghraph } from '@/styles/Fonts';
+import { MdOutlineEmail } from "react-icons/md";
+import { MdOutlinePassword } from "react-icons/md";
+import { playfair } from '@/styles/Fonts';
+// import sign from next-auth
+import { signIn } from 'next-auth/react';
 
 function page() {
     const [password, setpassword] = useState();
@@ -15,21 +19,21 @@ function page() {
     }
 
     return (
-        <div className='p-[60px] h-lvh'>
+        <div className='p-[60px] h-lvh flex flex-col items-center gap-0 justify-end '>
 
-            <div className=' h-fit w-[45%] m-auto rounded-lg shadow-md'>
-                <div className='p-8'>
+            <div className=' h-fit w-[40%]  rounded-lg shadow-md '>
+                <div className='p-8 '>
                     <div className='up '>
                         <p className='text-[20px]'>Hi, Welcome Back!</p>
                     </div>
                     <div className='google-facebooklogin flex gap-5 mt-4'>
-                        <div className='googlebtn flex items-center gap-2 border p-[6px] w-[100%] justify-center rounded-md'>
+                        <div onClick={() => signIn('google', { callbackUrl: '/dashboard' }) } className='googlebtn cursor-pointer flex items-center gap-2 border p-[6px] w-[100%] justify-center rounded-md bg-black text-white'>
                             <div className='w-[20px] h-[20px] '>
                                 <img src="/Google__G__logo.svg.png" alt="" className='w-full h-full object-cover' />
                             </div>
                             <p className='text-[13px]'>Sign in with Google</p>
                         </div>
-                        <div className='facebookbtn flex items-center gap-2 border p-[6px] w-[100%] justify-center rounded-md'>
+                        <div onClick={() => signIn('facebook', { callbackUrl: "/dashboard"}) } className='facebookbtn cursor-pointer flex items-center gap-2 border p-[6px] w-[100%] justify-center rounded-md'>
                             <div className='w-[20px] h-[20px] '>
                                 <img src="/Facebook.png" alt="" className='w-full h-full object-cover' />
                             </div>
@@ -45,7 +49,7 @@ function page() {
                         <div>
                             <label htmlFor="" className='text-[13px] mb-2 block'>Email Adress</label>
                             <div className='flex items-center gap-2 p-2 border rounded-md'>
-                                <EmailOutlinedIcon />
+                                <MdOutlineEmail />
                                 <input type="text" placeholder='Email' className='outline-none w-full text-[12px]' />
                             </div>
                         </div>
@@ -53,7 +57,7 @@ function page() {
                             <label htmlFor="" className='text-[13px] mb-2 block'>Password</label>
                             <div className='flex items-center justify-between gap-4 p-2 border rounded-md'>
                                 <div className='flex gap-2 w-[100%]'>
-                                    <LockOutlinedIcon />
+                                    <MdOutlinePassword />
                                     <input type={passwordvisible ? "text" : "password"} placeholder='Password' value={password} onChange={(e) => setpassword(e.target.value)} className='  grow outline-none  text-[12px]' />
                                 </div>
                                 <div>
@@ -62,17 +66,17 @@ function page() {
                                 </div>
                             </div>
                         </div>
-                        <button className='bg-[#1d5ce2] text-white p-3 rounded-md w-[100%] mt-2'>Log in</button>
+                        <button className='bg-[black] text-white p-3 rounded-md w-[100%] mt-2'>Log in</button>
 
                         <div className='buttom'>
-                            <div className='flex items-center justify-center mb-5 mt-3'>
+                            {/* <div className='flex items-center justify-center mb-5 mt-3'>
                                 <Link href="/" 
-                                    className={`${paraghraph.className} text-[14px] text-[#1d5ce2] underline`}
+                                    className={`${playfair.className} text-[14px] text-[#1d5ce2] underline`}
                                 >Forgot Password?</Link>
-                            </div>
+                            </div> */}
                             <div className="flex-grow border-t border-gray-200 mt-[2px]"></div>
                             <p className="text-center text-[14px] mt-3">Don't have an account? <Link href="/signup"
-                                className={`${paraghraph.className} text-[14px] text-[#1d5ce2] underline`}
+                                className={`${playfair.className} text-[14px] text-[#1d5ce2] underline`}
                             >Sign Up</Link></p>
                         </div>
                     </div>
