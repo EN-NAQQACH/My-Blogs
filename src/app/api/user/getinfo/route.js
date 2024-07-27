a7d8b586f314a45c75826906ff79b94fbee3fbb0
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-07-27 22:03:04 +0100

    getuserinfo logic

--- src/app/api/user/getinfo/route.js
+++ src/app/api/user/getinfo/route.js
@@ -1,24 +1,29 @@
 import { User } from "../../../../../server/models/User";
+import { Blog } from "../../../../../server/models/Blog";
 import connectDB from "../../../../../server/libs/Mongodb";
 import { NextResponse } from "next/server";
+import { create } from "domain";
 
 export const GET = async (req) => {
-    const { searchParams } = new URL(req.url);
-    const userId = searchParams.get('userId');
-  
-    if (!userId) {
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get('userId');
+
+  if (!userId) {
       return NextResponse.json({ error: "User ID is required" }, { status: 400 });
-    }
-  
-    try {
+  }
+
+  try {
       await connectDB();
-      const user = await User.findById(userId);
+      const user = await User.findById(userId).populate('blogs');
       if (!user) {
-        return NextResponse.json({ error: "User not found" }, { status: 404 });
+          return NextResponse.json({ error: "User not found" }, { status: 404 });
       }
-      return NextResponse.json(user);
-    } catch (error) {
-      console.error("Error fetching user:", error);
+    
+      const blogIds = user.blogs.map(blog => blog._id);
+      const blogs = await Blog.find({ _id: { $in: blogIds } });
+      return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+      console.error("Error fetching blogs:", error);
       return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-    }
-  };
+  }
+};
