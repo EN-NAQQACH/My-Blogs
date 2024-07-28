08aed47c72647a64b9769299f73c2280fefdd4c4
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-07-28 20:08:19 +0100

    changes in "add comment"

--- src/app/api/comment/add/route.js
+++ src/app/api/comment/add/route.js
@@ -1,33 +1,45 @@
-import {Comment} from "../../../../../server/models/Comment";
-import {Blog} from "../../../../../server/models/Blog";
+import { Comment } from "../../../../../server/models/Comment";
+import { Blog } from "../../../../../server/models/Blog";
 import connectDB from "../../../../../server/libs/Mongodb";
 import { NextResponse } from "next/server";
 
 
 export const POST = async (req) => {
 
-    const {blog_id, blog_author,comment, commented_by} = await req.json();
+    const { blog_id, blog_author, comment, commented_by, replyTo } = await req.json();
+    console.log("replyto", replyTo);
     try {
         await connectDB();
-        const newComment = new Comment({
+
+        // Create a new comment object
+        const newComment = {
             blog_id,
             blog_author,
             comment,
-            commented_by
-        });
-        await newComment.save().then(commentFile => {
-            let {comment,children} = commentFile;
-            Blog.findByIdAndUpdate({_id:blog_id},{$push:{"comments":commentFile._id}
-                , $inc: { "activity.total_comments": 1 }, "activity.total_parent_comments": 1
-            
-            }).then(
-                blog => {
-                    console.log("new comment created");
-                }
-            )
-        })
-        return NextResponse.json({newComment}, {status: 200});
+            commented_by,
+            parent: replyTo ? replyTo : undefined
+        };
+
+        const commentFile = await new Comment(newComment).save();
+        await Blog.findByIdAndUpdate(
+            blog_id,
+            {
+                $push: { comments: commentFile._id },
+                $inc: { "activity.total_comments": 1 },
+                $set: { "activity.total_parent_comments": replyTo ? 0 : 1 }
+            }
+        );
+        // If replyTo exists, update the parent comment's children array
+        if (replyTo) {
+            await Comment.findByIdAndUpdate(
+                replyTo,
+                { $push: { children: commentFile._id } }
+            );
+            console.log("Parent comment updated");
+        }
+
+        return NextResponse.json({ newComment }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({message: "Error adding comment"}, {status: 500});
+        return NextResponse.json({ message: "Error adding comment" }, { status: 500 });
     }
 }
